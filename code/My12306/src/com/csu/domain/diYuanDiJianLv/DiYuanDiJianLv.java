0492b1a402bad751e5e66fe229514506a280d7fe
WRZUP <wang_ruizhong@foxmail.com>
2016-09-18 15:00:55 +0800

    domain

--- code/My12306/src/com/csu/domain/diYuanDiJianLv/DiYuanDiJianLv.java
+++ code/My12306/src/com/csu/domain/diYuanDiJianLv/DiYuanDiJianLv.java
@@ -1,7 +1,63 @@
 package com.csu.domain.diYuanDiJianLv;
 
 /**
- * Created by 温 睿诚 on 2016/9/18/0018.
+ * Created by Asus_ on 2016/9/13.
  */
 public class DiYuanDiJianLv {
+    private String dYDJId;//地缘递减率编号
+    private String dYDJFrom;//区段FROM
+    private String dYDJTo;//区段TO
+    private double dYDJRate;//地缘递减率
+    private double priceRate;//票价率
+
+    public DiYuanDiJianLv() {
+    }
+
+    public DiYuanDiJianLv(String dYDJId, String dYDJFrom, String dYDJTo, double dYDJRate, double priceRate) {
+        this.dYDJId = dYDJId;
+        this.dYDJFrom = dYDJFrom;
+        this.dYDJTo = dYDJTo;
+        this.dYDJRate = dYDJRate;
+        this.priceRate = priceRate;
+    }
+
+    public String getdYDJId() {
+        return dYDJId;
+    }
+
+    public void setdYDJId(String dYDJId) {
+        this.dYDJId = dYDJId;
+    }
+
+    public String getdYDJFrom() {
+        return dYDJFrom;
+    }
+
+    public void setdYDJFrom(String dYDJFrom) {
+        this.dYDJFrom = dYDJFrom;
+    }
+
+    public String getdYDJTo() {
+        return dYDJTo;
+    }
+
+    public void setdYDJTo(String dYDJTo) {
+        this.dYDJTo = dYDJTo;
+    }
+
+    public double getdYDJRate() {
+        return dYDJRate;
+    }
+
+    public void setdYDJRate(double dYDJRate) {
+        this.dYDJRate = dYDJRate;
+    }
+
+    public double getPriceRate() {
+        return priceRate;
+    }
+
+    public void setPriceRate(double priceRate) {
+        this.priceRate = priceRate;
+    }
 }
--- code/My12306/src/com/csu/domain/line/Line.java
+++ code/My12306/src/com/csu/domain/line/Line.java
@@ -1,5 +1,75 @@
 package com.csu.domain.line;
 
+import sun.util.calendar.BaseCalendar;
 
+/**
+ * Created by Asus_ on 2016/9/13.
+ */
 public class Line {
+    private String lineId;//线路编号
+    private String trainId;//车次
+    private String stationName;//车站名
+    private int distance;//里程
+    private BaseCalendar.Date fromTime;//到站时间
+    private String lastTime;//停留时间
+
+    public Line() {
+    }
+
+    public Line(String lineId, String trainId, String stationName, int distance, BaseCalendar.Date fromTime, String lastTime) {
+        this.lineId = lineId;
+        this.trainId = trainId;
+        this.stationName = stationName;
+        this.distance = distance;
+        this.fromTime = fromTime;
+        this.lastTime = lastTime;
+    }
+
+    public String getLineId() {
+        return lineId;
+    }
+
+    public void setLineId(String lineId) {
+        this.lineId = lineId;
+    }
+
+    public String getTrainId() {
+        return trainId;
+    }
+
+    public void setTrainId(String trainId) {
+        this.trainId = trainId;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public BaseCalendar.Date getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(BaseCalendar.Date fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public String getLastTime() {
+        return lastTime;
+    }
+
+    public void setLastTime(String lastTime) {
+        this.lastTime = lastTime;
+    }
 }
